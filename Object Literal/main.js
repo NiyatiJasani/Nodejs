673ebb3cdfc8868a0769d9cdf7baf34a28525ba6
NiyatiJasani <niyatijasani6@gmail.com>
2017-01-24 13:12:24 +0530

    Nodejs with Object Literals

---
+++ Object Literal/main.js
@@ -0,0 +1,5 @@
+var m = require('./techlab-math');
+
+console.log(m.add(10, 20));
+console.log(m.subtract(5, 2));
+console.log(m.divide(20 / 5));
---
+++ Object Literal/techlab-math.js
@@ -0,0 +1,14 @@
+//Object  Literal
+
+module.exports = {
+    add: function (a, b) {
+        return a + b;
+    },
+    subtract: function (a, b) {
+        return a - b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    }
+
+};
